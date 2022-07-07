243a7c83e3e62df2784d9ed4216368f39abd3ec0
gudako <lostbelt@protonmail.com>
2022-07-06 23:37:50 -0700

    getting started

--- handbook.php
+++ handbook.php
@@ -5,9 +5,6 @@
 <body>
 <?php include 'mainbar.php'; include 'lowbar.php';?>
 <link href="css/handbook.css" rel="stylesheet"/>
-<div id="back">
 
-    <img src="img/pages/back/handbook_back.png">
-</div>
 
 </body>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php
-    require 'initialize.php';
+<?php require 'initialize.php';
     if(isset($_GET['lang'])) {
         setcookie('lang', $_GET['lang'], time()+(60*60*24*365));
         header('Location: index.php');
@@ -10,7 +9,16 @@
     <title><?php echo text('game_name');?></title>
 </head>
 <body style="margin: 0; padding: 0">
-<?php include 'mainbar.php'?>
+<?php include 'mainbar.php'; include 'lowbar.php';?>
+<link href="css/mainpage.css" rel="stylesheet"/>
+<div id="bar1">
+    <div id="bar1_clip_img"> <img src="img/pages/mainpage1.jpg"></div>
+    <div id="bar1_advocate"><div>Help us FIGHT BACK the traveller</div></div>
+</div>
+<div id="bar2">
+    We are the hilichurls, in Teyvat.
+</div>
+<div id="bar3">
 
-
+</div>
 </body>
