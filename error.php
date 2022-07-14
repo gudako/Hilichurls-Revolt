d7bfd6e95f68ef174b8b4ddb122aea6aaef210b9
gudako <lostbelt@protonmail.com>
2022-07-14 13:40:02 -0700

    another backup
    hold on with it...

--- error.php
+++ error.php
@@ -1,4 +1,17 @@
-<?php http_response_code(500); ?>
+<?php
+if($_SERVER['REQUEST_METHOD']!=='GET'){
+    http_response_code(400);
+    die();
+}
+$lang = $_GET['lang'] ?? 'en';
+$logcode = $_GET['logcode'] ?? null;
+$msg = $_GET['msg'] ?? null;
+if(!($logcode===null xor $msg===null)){
+    http_response_code(400);
+    die();
+}
+else http_response_code(500);
+?>
 <head>
     <title>500 Internal Server Error</title>
 </head>
@@ -35,9 +48,15 @@
         top: max(calc(106.667px - 11.3333vw),16.0006px);
         left: calc(-270.4px + 97.8vw);
     }
-    #texts{
-        margin:40px 30px 0 30px;
+    .texts{
+        margin:11px 30px 0 30px;
         font-size: min(calc(13.3333px + 0.533333vw),17.599964px);
+    }
+    a{
+        display: block;
+        font-size: 14px;
+        margin-top: 12px;
+        margin-left: 4px;
     }
     @media only screen and (max-width: 800px) {
         #topic>img{
@@ -49,15 +68,65 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC&display=swap" rel="stylesheet">
-
 <div id="topic">
-    <div id="stuck">WE ARE STUCK!</div>
-    <div>That saying, we got those bugs in file......</div>
+    <div id="stuck">
+        <?php
+        if($lang === 'en')
+            echo "WE ARE STUCK!";
+        elseif ($lang ==='zh')
+            echo "服务器错误......";
+        ?>
+    </div>
+    <div>
+        <?php
+        if($lang === 'en')
+            echo "That saying, we got those bugs in file......";
+        elseif ($lang ==='zh')
+            echo "指的是，这些文件里有一些奇怪的BUG......";
+        ?>
+    </div>
     <img src="img/pages/bug.png">
 </div>
-<div id="texts">
-    It's not your problem. It's our fault. There are something wrong in the server.<br>
-    The error is logged, and we will deal with it as soon as possible.
+<a href="">chang lang</a>
+<div class="texts" style="margin-top:40px;">
+    <?php
+    if($lang === 'en')
+        echo "It's not your problem. It's our fault. There are something buggy in the server.";
+    elseif ($lang ==='zh')
+        echo "这不是你的问题——是我们的问题。服务器里可能出了一些BUG......";
+    ?>
+</div>
+<div class="texts" style="<?php if($logcode===null)echo 'display:none';?>">
+    <?php
+    if($lang === 'en')
+        echo "The error is successfully logged to the database, and we will deal with it as soon as possible.";
+    elseif ($lang ==='zh')
+        echo "错误信息已经被成功发送到了数据库中，我们将尽快修复该问题。";
+    ?>
+</div>
+<div class="texts" style="<?php if($msg===null)echo 'display:none';?>">
+    <?php
+    if($lang === 'en')
+        echo "Meanwhile, we are not able to log the error data to the database :(";
+    elseif ($lang ==='zh')
+        echo "同时，我们未能成功将此次错误信息发送至数据库 :(";
+    ?>
+</div>
+<div class="texts">
+    <?php
+    if($lang === 'en')
+        echo "If the problem persists, you can get in contact with us send the following code to email \"lostbelt@protonmail.com\", we'll reply you fast.";
+    elseif ($lang ==='zh')
+        echo "如果该问题持续存在，你可以把下面的代码发送到邮箱 \"lostbelt@protonmail.com\" 中，来和我们获得联系。我们会在第一时间内处理此问题。";
+    ?>
+</div>
+<div class="texts" style="font-size: 34px;<?php if($logcode===null)echo 'display:none';?>">
+    <?php echo 'ERR'.$logcode;?></div>
+<div class="texts" style="border:1px #3b0909 solid;margin:20px;padding:10px;color:darkslategrey;
+<?php if($msg===null)echo 'display:none';?>"><?php echo $msg;?></div>
+<div class="texts">
+    <?php if($lang === 'en') echo "Sincere apology to all the inconveniences we may have caused to you.";
+    elseif ($lang ==='zh') echo "同时，我们诚挚地为所有可能对您造成的不便表示抱歉。";?>
 </div>
 <?php
 
